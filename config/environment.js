810b9635b749ba9c421ac7f426eabf675f2c4a55
Gods-Blessing <mohitadhana2569@gmail.com>
2022-12-06 19:14:21 +0530

    changes

--- config/environment.js
+++ config/environment.js
@@ -1,17 +1,1 @@
 
-
-const development = {
-    name: "development",
-    asset_path: './assets',
-    session_cookie_key: 'lazysloth',
-    db: 'issuetracker',
-    db_password: 'BugtrackerDevmohit'
-}
-
-
-const production = {
-    name:"production"
-}
-
-
-module.exports = development;
